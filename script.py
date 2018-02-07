49662b60fcea60d5a5a9fb3ea0c487b2993d8732
YorkCpE <jbforsyth@ycp.edu>
2018-02-07 14:07:03 -0500

    Script appears to work; confirming plots

--- script.py
+++ script.py
@@ -174,7 +174,7 @@ if __name__ == "__main__":
 
 	learning_rate = 0.0025
 	lambda_loss_amount = 0.0015
-	training_iters = training_data_count * 10  # Loop 300 times on the dataset
+	training_iters = training_data_count * 300  # Loop 300 times on the dataset
 	batch_size = 1500
 	display_iter = 30000  # To show test set accuracy during training
 
@@ -283,3 +283,81 @@ if __name__ == "__main__":
 	      "Batch Loss = {}".format(final_loss) + \
 	      ", Accuracy = {}".format(accuracy))
 
+	# Training is good, but having visual insight is even better:
+	# (Inline plots: )
+	#matplotlib inline
+
+	font = {
+	    'family' : 'Bitstream Vera Sans',
+	    'weight' : 'bold',
+	    'size'   : 18
+	}
+	matplotlib.rc('font', **font)
+
+	width = 12
+	height = 12
+	plt.figure(figsize=(width, height))
+
+	indep_train_axis = np.array(range(batch_size, (len(train_losses)+1)*batch_size, batch_size))
+	plt.plot(indep_train_axis, np.array(train_losses),     "b--", label="Train losses")
+	plt.plot(indep_train_axis, np.array(train_accuracies), "g--", label="Train accuracies")
+
+	indep_test_axis = np.append(
+	    np.array(range(batch_size, len(test_losses)*display_iter, display_iter)[:-1]),
+	    [training_iters]
+	)
+	plt.plot(indep_test_axis, np.array(test_losses),     "b-", label="Test losses")
+	plt.plot(indep_test_axis, np.array(test_accuracies), "g-", label="Test accuracies")
+
+	plt.title("Training session's progress over iterations")
+	plt.legend(loc='upper right', shadow=True)
+	plt.ylabel('Training Progress (Loss or Accuracy values)')
+	plt.xlabel('Training iteration')
+
+	plt.show()
+
+	# Results
+	# And finally, the multi-class confusion matrix and metrics!
+
+	predictions = one_hot_predictions.argmax(1)
+
+	print("Testing Accuracy: {}%".format(100*accuracy))
+
+	print("")
+	print("Precision: {}%".format(100*metrics.precision_score(y_test, predictions, average="weighted")))
+	print("Recall: {}%".format(100*metrics.recall_score(y_test, predictions, average="weighted")))
+	print("f1_score: {}%".format(100*metrics.f1_score(y_test, predictions, average="weighted")))
+
+	print("")
+	print("Confusion Matrix:")
+	confusion_matrix = metrics.confusion_matrix(y_test, predictions)
+	print(confusion_matrix)
+	normalised_confusion_matrix = np.array(confusion_matrix, dtype=np.float32)/np.sum(confusion_matrix)*100
+
+	print("")
+	print("Confusion matrix (normalised to % of total test data):")
+	print(normalised_confusion_matrix)
+	print("Note: training and testing data is not equally distributed amongst classes, ")
+	print("so it is normal that more than a 6th of the data is correctly classifier in the last category.")
+
+	# Plot Results: 
+	width = 12
+	height = 12
+	plt.figure(figsize=(width, height))
+	plt.imshow(
+	    normalised_confusion_matrix, 
+	    interpolation='nearest', 
+	    cmap=plt.cm.rainbow
+	)
+	plt.title("Confusion matrix \n(normalised to % of total test data)")
+	plt.colorbar()
+	tick_marks = np.arange(n_classes)
+	plt.xticks(tick_marks, LABELS, rotation=90)
+	plt.yticks(tick_marks, LABELS)
+	plt.tight_layout()
+	plt.ylabel('True label')
+	plt.xlabel('Predicted label')
+	plt.show()
+
+	#kill tensorflow
+	sess.close()
